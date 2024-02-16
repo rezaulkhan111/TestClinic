5479899e827360ef58860b32e1efb0cb50458448
rezaulkhan111 <rezaulkhan111@hotmail.com>
2024-02-17 00:31:46 +0600

    add adapter

---
+++ app/src/main/java/com/example/testclinic/adapter/ClinicAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.testclinic.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.testclinic.databinding.ListItemClinicBinding
+import com.example.testclinic.model.Clinic
+
+class ClinicAdapter :
+    RecyclerView.Adapter<ClinicAdapter.ClinicVH>() {
+
+    private var listRepositoryDet: MutableList<Clinic> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClinicVH {
+        return ClinicVH(
+            ListItemClinicBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return if (listRepositoryDet.size > 0) {
+            listRepositoryDet.size
+        } else {
+            0
+        }
+    }
+
+    fun setRepository(listRepository: MutableList<Clinic>) {
+        listRepositoryDet = listRepository
+    }
+
+    override fun onBindViewHolder(holder: ClinicVH, position: Int) {
+        try {
+            holder.onBind(position)
+//            holder.itemView.setOnClickListener {
+//                val accountInformation = listRepositoryDet[position]
+//                repositoryCallback.repositoryItemClick(accountInformation, position)
+//            }
+        } catch (_: Exception) {
+        }
+    }
+
+    inner class ClinicVH(val binding: ListItemClinicBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun onBind(position: Int) {
+            val repoModel = listRepositoryDet[position]
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/testclinic/model/Clinic.kt
@@ -0,0 +1,18 @@
+package com.example.testclinic.model
+
+class Clinic {
+//    @SerializedName("id")
+//    var id: Int? = null
+//
+//    @SerializedName("name")
+//    var name: String? = null
+//
+//    @SerializedName("email")
+//    var email: String? = null
+//
+//    @SerializedName("gender")
+//    var gender: String? = null
+//
+//    @SerializedName("status")
+//    var status: String? = null
+}
