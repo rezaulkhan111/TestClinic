2d356cbcedf00179b1325230a024318bae822777
rezaulkhan111 <rezaulkhan111@hotmail.com>
2024-02-17 00:48:24 +0600

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,19 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("androidx.navigation.safeargs")
 }
 
 android {
     namespace = "com.example.testclinic"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.testclinic"
         minSdk = 24
+        //noinspection OldTargetApi
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -41,11 +45,26 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.7.0")
+
+    //jetpack navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:2.7.7")
+
+    //hilt
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-compiler:2.44")
 }
--- app/src/main/java/com/example/testclinic/model/Clinic.kt
+++ app/src/main/java/com/example/testclinic/model/Clinic.kt
@@ -1,18 +1,20 @@
 package com.example.testclinic.model
 
+import com.google.gson.annotations.SerializedName
+
 class Clinic {
-//    @SerializedName("id")
-//    var id: Int? = null
-//
-//    @SerializedName("name")
-//    var name: String? = null
-//
-//    @SerializedName("email")
-//    var email: String? = null
-//
-//    @SerializedName("gender")
-//    var gender: String? = null
-//
-//    @SerializedName("status")
-//    var status: String? = null
+    @SerializedName("id")
+    var id: Int? = null
+
+    @SerializedName("name")
+    var name: String? = null
+
+    @SerializedName("email")
+    var email: String? = null
+
+    @SerializedName("gender")
+    var gender: String? = null
+
+    @SerializedName("status")
+    var status: String? = null
 }
